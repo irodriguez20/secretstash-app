71826872c835b4401f655f3d8133e60a89bc89c1
India Rodriguez <carnes.s.ia17@gmail.com>
2020-01-25 12:19:42 -0700

    fixed grammer errors on Landing Page

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -19,19 +19,19 @@ class LandingPage extends Component {
                     <section className='LandingPage__why'>
                         <h3>Why Secret Stash</h3>
                         <p> Recipes are memories captured in food. They are family heirlooms passed down from generation to generation. Losing
-                          a recipe, is losing something very special and sentimental. Recipes on paper are a thing of the past. As Generations
+                          a recipe is losing something very special and sentimental. Recipes on paper are a thing of the past. As generations
                           continue to grow technologically, recipes on paper will become obsolete. We need a way to store our family history with
 				a lowered risk of loss.</p>
                     </section>
                     <section className='LandingPage__recycle'>
                         <h3>Save the Trees</h3>
                         <h4><FontAwesomeIcon icon="recycle" /> <FontAwesomeIcon icon="tree" /></h4>
-                        <p>Research shows the average household throws away 13,000 pieces of paper a year. Storing your recipes online eliminates the use of paper, decreasing the amount of trees cut down and paper wasted.</p>
+                        <p>Research shows the average household throws away 13,000 pieces of paper a year. Storing your recipes online eliminates the use of paper; decreasing the amount of trees cut down and paper wasted.</p>
                     </section>
                     <section className='LandingPage__organized'>
                         <h3>Organized in One Place</h3>
                         <img src={RecipePageMainScreenShot} alt="screenshot of the recipe main screen" />
-                        <p>My family and I are notorious for having recipes saved everywhere. From bookmarks on safari, stashed in recipe boxes, folders, even saved as images on our phones. <br /> With Secret Stash all of your recipes are neatly organized on one app. Create folders for the different categories, edit and delete recipes as desired. </p>
+                        <p>My family and I are notorious for having recipes saved everywhere. From bookmarks on Safari, stashed in recipe boxes, folders, even saved as images on our phones. <br /> With Secret Stash all of your recipes are neatly organized on one app. Create folders for the different categories, edit and delete recipes as desired. </p>
                     </section>
                     {/*To come when I add authorization and user accounts. <section className='LandingPage__share'>
                         <h3>Share with your Family Members</h3>
